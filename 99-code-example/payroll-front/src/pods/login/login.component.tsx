2f69bad8dbbb1864bf154b3c1d1ee69b861d03b6
juanpms2 <juanpablo@lemoncode.net>
2021-09-07 08:41:13 +0200

    first commit added login form

--- 99-code-example/payroll-front/src/pods/login/login.component.tsx
+++ 99-code-example/payroll-front/src/pods/login/login.component.tsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import * as classes from './login.styles';
 
 export const LoginComponent: React.FunctionComponent = () => {
-  return <h1>Login component</h1>;
+  return (
+    <div className={classes.root}>
+      <div className={classes.container}>
+        <form className={classes.form}>
+          <div className={classes.fields}>
+            <TextField label="Email" type="email" />
+            <TextField label="Password" type="password" />
+          </div>
+          <Button variant="contained" color="primary">
+            Login
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
 };
