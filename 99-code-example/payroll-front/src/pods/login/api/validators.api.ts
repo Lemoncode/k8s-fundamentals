8916eb5008987dd19b2962d84cae91762d023ea0
juanpms2 <juanpablo@lemoncode.net>
2021-09-07 13:46:15 +0200

    added async validation

--- 99-code-example/payroll-front/src/pods/login/api/validators.api.ts
+++ 99-code-example/payroll-front/src/pods/login/api/validators.api.ts
@@ -1,4 +1,13 @@
 import axios from 'axios';
+import { FieldValidationFunctionAsync } from '@lemoncode/fonk';
 
-export const existEmail = async (email: string): Promise<boolean> =>
-  Promise.resolve(email === 'email@error.com' ? true : false);
+export const existEmail: FieldValidationFunctionAsync = async ({ value }) => {
+  const validationResult = {
+    type: 'EMAIL_EXISTS',
+    succeeded: false,
+    message: 'The email exists',
+  };
+  return value === 'email@error.com'
+    ? validationResult
+    : { ...validationResult, succeeded: true, message: '' };
+};
--- 99-code-example/payroll-front/src/pods/login/login.validations.ts
+++ 99-code-example/payroll-front/src/pods/login/login.validations.ts
@@ -1,10 +1,11 @@
 import { Validators } from '@lemoncode/fonk';
 import { createFormikValidation } from '@lemoncode/fonk-formik';
+import { existEmail } from './api/validators.api';
 import { passValidator } from './validations/pass-security-policy';
 
 const validationSchema = {
   field: {
-    email: [Validators.required, Validators.email],
+    email: [Validators.required, Validators.email, existEmail],
     password: [Validators.required, passValidator],
   },
 };
