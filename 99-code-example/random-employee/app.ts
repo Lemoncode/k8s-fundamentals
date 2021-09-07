81520825dcdedec9a0b849529c4e6c8705afa46f
Jaime Salas Zancada <jaimesalaszancada@Jaimes-MacBook-Pro.local>
2021-09-07 16:04:53 +0200

    added health probe

--- 99-code-example/random-employee/app.ts
+++ 99-code-example/random-employee/app.ts
@@ -8,6 +8,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use('/employees', employeeRouter);
 
+app.get('/health', (_, res) => {
+    res.status(200).json('ok');
+});
+
 app.listen(config.http.port, () => {
     console.log(`Application running on ${config.http.port}`);
 });
