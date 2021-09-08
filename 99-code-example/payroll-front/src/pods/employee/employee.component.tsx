65ff73ba3569eeb001b8cf7ec7bd0ea12aca7ce2
juanpms2 <juanpablo@lemoncode.net>
2021-09-08 17:12:52 +0200

    added chart

--- 99-code-example/payroll-front/src/pods/employee/employee.component.tsx
+++ 99-code-example/payroll-front/src/pods/employee/employee.component.tsx
@@ -7,6 +7,7 @@ import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Collapse from '@material-ui/core/Collapse';
+import ChartComponent from './components/charts/chart.component';
 import { Employee } from './employee.vm';
 
 interface Props {
@@ -16,6 +17,12 @@ interface Props {
 export const EmployeeComponent: React.FunctionComponent<Props> = ({
   employee,
 }) => {
+  const dataChart = employee.payrollInfo.payrollList
+    .map((payroll) => ({
+      label: payroll.date.getFullYear().toString(),
+      amount: payroll.amount,
+    }))
+    .sort((a, b) => (a.label > b.label ? 1 : -1));
   const [expanded, setExpanded] = React.useState(false);
 
   const handleExpandClick = () => {
@@ -23,61 +30,70 @@ export const EmployeeComponent: React.FunctionComponent<Props> = ({
   };
 
   return (
-    <Card variant="outlined">
-      <CardContent>
-        <Typography variant="h5" component="h2">
-          {`${employee.id} ${employee.name} ${employee.username}`}
-        </Typography>
-        <Typography color="textSecondary">{employee.address.street}</Typography>
-        <Typography variant="body2" component="p">
-          {employee.company.name}
-        </Typography>
-        <Typography variant="body2" component="p">
-          {employee.email}
-        </Typography>
-        <Typography variant="body2" component="p">
-          {employee.phone}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small" color="primary">
-          Show chart
-        </Button>
-        <IconButton
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <ExpandMoreIcon />
-        </IconButton>
-      </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        {employee.payrollInfo.payrollList.map((payroll) => (
-          <CardContent key={payroll.id}>
-            <Typography variant="h5" component="h2">
-              {payroll.id}
-            </Typography>
-            <Typography color="textSecondary">
-              {payroll.date.toString()}
-            </Typography>
-            <Typography variant="body2" component="p">
-              {payroll.type}
-            </Typography>
-            <Typography variant="body2" component="p">
-              {payroll.cost}
-            </Typography>
-            <Typography variant="body2" component="p">
-              {payroll.amount}
-            </Typography>
-            <Typography variant="body2" component="p">
-              {payroll.accrual}
-            </Typography>
-            <Typography variant="body2" component="p">
-              {payroll.withhold}
-            </Typography>
-          </CardContent>
-        ))}
-      </Collapse>
-    </Card>
+    <>
+      <Card variant="outlined">
+        <CardContent>
+          <Typography variant="h5" component="h2">
+            {`${employee.id} ${employee.name} ${employee.username}`}
+          </Typography>
+          <Typography color="textSecondary">
+            {employee.address.street}
+          </Typography>
+          <Typography variant="body2" component="p">
+            {employee.company.name}
+          </Typography>
+          <Typography variant="body2" component="p">
+            {employee.email}
+          </Typography>
+          <Typography variant="body2" component="p">
+            {employee.phone}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small" color="primary">
+            Show chart
+          </Button>
+          <IconButton
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            <ExpandMoreIcon />
+          </IconButton>
+        </CardActions>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          {employee.payrollInfo.payrollList.map((payroll) => (
+            <CardContent key={payroll.id}>
+              <Typography variant="h5" component="h2">
+                {payroll.id}
+              </Typography>
+              <Typography color="textSecondary">
+                {payroll.date.toString()}
+              </Typography>
+              <Typography variant="body2" component="p">
+                {payroll.type}
+              </Typography>
+              <Typography variant="body2" component="p">
+                {payroll.cost}
+              </Typography>
+              <Typography variant="body2" component="p">
+                {payroll.amount}
+              </Typography>
+              <Typography variant="body2" component="p">
+                {payroll.accrual}
+              </Typography>
+              <Typography variant="body2" component="p">
+                {payroll.withhold}
+              </Typography>
+            </CardContent>
+          ))}
+        </Collapse>
+      </Card>
+
+      <ChartComponent
+        dataChart={dataChart}
+        title={`${employee.name} ${employee.username}`}
+      />
+    </>
   );
 };
