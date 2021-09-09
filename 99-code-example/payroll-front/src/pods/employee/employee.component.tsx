1865baa0a1ba08368fb7b67504a3dbb7c5efeda2
juanpms2 <juanpablo@lemoncode.net>
2021-09-09 13:49:54 +0200

    refactor

--- 99-code-example/payroll-front/src/pods/employee/employee.component.tsx
+++ 99-code-example/payroll-front/src/pods/employee/employee.component.tsx
@@ -11,7 +11,7 @@ import IconButton from '@material-ui/core/IconButton';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Collapse from '@material-ui/core/Collapse';
 import { routes } from 'core/router';
-import ChartComponent from './components/charts/chart.component';
+import ChartComponent from 'common/components/charts/chart.component';
 import { Employee } from './employee.vm';
 
 interface Props {
