76c72507fd381f20b95c2ab90e4ba141c42a2aa6
juanpms2 <juanpablo@lemoncode.net>
2021-09-10 11:33:41 +0200

    refactor

--- 99-code-example/payroll-front/src/pods/sign-in/sign-in.styles.ts
+++ 99-code-example/payroll-front/src/pods/sign-in/sign-in.styles.ts
@@ -17,22 +17,6 @@ export const container = css`
   flex-direction: column;
 `;
 
-export const form = css`
-  display: flex;
-  flex-grow: 1;
-  flex-direction: column;
-  justify-content: space-around;
-  row-gap: 20px;
-  width: 100%;
-`;
-
-export const fields = css`
-  display: flex;
-  flex-direction: column;
-  row-gap: 20px;
-  width: 100%;
-`;
-
 export const error = css`
   color: red;
 `;
