20e4c33fc0087608f314e21acf345c1334edf0d3
JaimeSalas <jaime.salas@lemoncode.net>
2021-10-18 17:08:14 +0200

    logger updated

--- 99-code-example/random-employee/logger.ts
+++ 99-code-example/random-employee/logger.ts
@@ -4,8 +4,22 @@ export interface FileWriter {
   writeToFile: (input: string) => Promise<void>;
 }
 
+const logsExists = async (): Promise<Boolean> => {
+  const directoryContent = await fs.readdir(`${__dirname}`);
+  const logsMatches = directoryContent.filter((c) => c === 'logs');
+  return logsMatches.length > 0;
+};
+
+const createLogsDirectory = async (): Promise<void> => {
+  const logsCreated = await logsExists();
+  if (!logsCreated) {
+    console.log('Creating logs path');
+    await fs.mkdir(`${__dirname}/logs`);
+  }
+}
+
 export const loggerFactory = async (): Promise<FileWriter> => {
-  await fs.mkdir(`${__dirname}/logs`);
+  await createLogsDirectory();
   const logsPath = `${__dirname}/logs/log`
 
   return {
